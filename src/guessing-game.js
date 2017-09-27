2076186035dfed0307b048b118c98be1479f6907
Anna Bogdanova <espro@mail.ru>
2017-09-27 11:41:08 +0300

    Guessing game methods implemented

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,24 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+      this.minRange = min;
+      this.maxRange = max;
     }
 
     guess() {
-
+      var guessTemp = this.maxRange - this.minRange;
+      guessTemp = guessTemp/2;
+      guessTemp = Math.ceil(guessTemp) + this.minRange;
+      this.guessNum = guessTemp;
+      return this.guessNum;
     }
 
     lower() {
-
+    this.maxRange = this.guessNum;
     }
 
     greater() {
-
+    this.minRange = this.guessNum;
     }
 }
 
